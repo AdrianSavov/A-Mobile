b391d155fd06468d4b6edcf335aac34edbf07a23
Adrian Savov <skandalno@icloud.com>
2023-11-30 11:19:18 +0200

    Added advertisement component

--- react/src/Components/Home/Home.jsx
+++ react/src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Carousel from './Carousel/Carousel';
+import Advertisement from './Аdvertisement/Аdvertisement';
 
 export default function Home () {
 
     return (
         <>
         <Carousel />
-     
+        <Advertisement />
         </>
     )
 }
