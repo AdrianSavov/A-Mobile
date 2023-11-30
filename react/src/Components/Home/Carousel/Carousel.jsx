c40506c1924c2373638dfe91dc705eb5835426cd
Adrian Savov <skandalno@icloud.com>
2023-11-30 10:25:27 +0200

    Added carousel component
    Added Carousel component and it's style in the main Home component. Added some pictures as assets.

---
+++ react/src/Components/Home/Carousel/Carousel.jsx
@@ -0,0 +1,57 @@
+import Carousel from "react-bootstrap/Carousel";
+import "./Carousel.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function CarouselItem() {
+  return (
+    <div className="carousel-container">
+      <div className="carousel-container-text">
+        <h1 className="carousel-container-text-header">BLACK FRIDAY</h1>
+        <p className="carousel-container-text-p">UP TO</p>
+        <p className="carousel-container-text-p-percent">-70%</p>
+        <p className="carousel-container-text-p">OFF!</p>
+      </div>
+        <Carousel fade>
+          <Carousel.Item interval={3000}>
+            <img
+              className="carousel-img"
+              text="First slide"
+              src="src/assets/office.jpg"
+            />
+            <Carousel.Caption>
+              <h1 className="fade-in">Why to choice us?</h1>
+              <p className="fade-in">
+                Inovative solutions. Empower your mobile experience with our
+                cutting-edge services.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              className="carousel-img"
+              text="Second slide"
+              src="src/assets/work-2.png"
+            />
+            <Carousel.Caption>
+              <h1 className="fade-in">Why we are better?</h1>
+              <p className="fade-in">
+                24/7 customer support. Reaction in just 2 hours!
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              className="carousel-img"
+              text="4 slide"
+              src="src/assets/work-3.png"
+            />
+            <Carousel.Caption>
+              <h1 className="fade-in">Excellent network coverage!</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+  );
+}
+
+export default CarouselItem;
