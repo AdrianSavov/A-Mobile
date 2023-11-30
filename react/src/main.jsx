eea1823f22e64e5bd511bff25be0e9e6f5bdc52b
Adrian Savov <skandalno@icloud.com>
2023-11-30 22:18:13 +0200

    fix: removed inndex.css

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './index.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
