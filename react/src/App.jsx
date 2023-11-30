fcceba3cea741b9159d009117c9112152bbe609e
Adrian Savov <skandalno@icloud.com>
2023-11-30 11:37:32 +0200

    Added devices page, fetch devices, device details and Routes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,25 @@
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
+
+import { Routes, Route } from 'react-router-dom';
+
+
+import SmartphoneList from "./components/devices/SmartphoneList";
+import CellPhoneList from './components/devices/CellPhoneList';
+import SmartWatchList from './components/devices/SmartWatchList';
+
 function App() {
 
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="smartphones" element={<SmartphoneList />} />
+          <Route path="cellphones" element={<CellPhoneList />} />
+          <Route path="smartwatches" element={<SmartWatchList />} />
+      </Routes>
       <Footer />
     </>
   )
