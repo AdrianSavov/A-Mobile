2bec4d6fd325120df69df977b5fd427c8f613458
Adrian Savov <skandalno@icloud.com>
2023-12-02 11:59:18 +0200

    feat: Added auth provider

---
+++ react/src/authProvider/Auth.jsx
@@ -0,0 +1,44 @@
+// AuthContext.js
+import React, { createContext, useContext, useReducer } from 'react';
+
+const AuthStateContext = createContext();
+const AuthDispatchContext = createContext();
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, isAuthenticated: true, user: action.payload };
+    case 'LOGOUT':
+      return { ...state, isAuthenticated: false, user: null };
+    default:
+      return state;
+  }
+};
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, { isAuthenticated: false, user: null });
+
+  return (
+    <AuthStateContext.Provider value={state}>
+      <AuthDispatchContext.Provider value={dispatch}>
+        {children}
+      </AuthDispatchContext.Provider>
+    </AuthStateContext.Provider>
+  );
+};
+
+export const useAuthState = () => {
+  const context = useContext(AuthStateContext);
+  if (!context) {
+    throw new Error('useAuthState must be used within an AuthProvider');
+  }
+  return context;
+};
+
+export const useAuthDispatch = () => {
+  const context = useContext(AuthDispatchContext);
+  if (!context) {
+    throw new Error('useAuthDispatch must be used within an AuthProvider');
+  }
+  return context;
+};
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthProvider } from "./authProvider/Auth.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
