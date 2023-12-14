d2f7c45623410724509531d6b25a3b309c8ec3e0
Adrian Savov <skandalno@icloud.com>
2023-12-14 14:08:28 +0200

    refactor: not found page

--- app/src/Components/NotFound/NotFound.jsx
+++ app/src/Components/NotFound/NotFound.jsx
@@ -1,27 +1,15 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NotFound.css";
 
-const NotFoundPage = () => {
+const NotFound = () => {
   return (
-    <Container className="mt-5">
-      <Row>
-        <Col className="text-center">
-          <h1 className="display-1">404</h1>
-          <h2 className="display-2">Page Not Found</h2>
-          <p className="lead">
-            Oops! The page you are looking for might be in another galaxy.
-          </p>
-        </Col>
-      </Row>
-      <div className="home-btn">
-        <Link to="/">
-          <Button variant="primary">Go Home</Button>
-        </Link>
-      </div>
-    </Container>
+    <div className="container">
+      <h1 className="heading">404</h1>
+      <p className="text">Not Found</p>
+      <p className="text">The page you are looking for might be in another universe.</p>
+      <Link to="/" className="link">Go Home</Link>
+    </div>
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
